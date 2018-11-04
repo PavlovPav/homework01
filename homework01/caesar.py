a636566d2ddfd94b488ebfdc3cebb26a36cb2085
PavlovPav <lokimean@mail.ru>
2018-11-04 18:43:28 +0300

    Реализована функция encrypt_caesar()

---
+++ homework01/caesar.py
@@ -0,0 +1,27 @@
+def encrypt_caesar(plaintext):
+    """
+       Encrypts plaintext using a Caesar cipher.
+
+       >>> encrypt_caesar("PYTHON")
+       'SBWKRQ'
+       >>> encrypt_caesar("python")
+       'sbwkrq'
+       >>> encrypt_caesar("Python3.6")
+       'Sbwkrq3.6'
+       >>> encrypt_caesar("")
+       ''
+       """
+    res = ''
+    for c in plaintext:
+        if ord('A') <= ord(c) <= ord ('Z'):
+            k = ord(c) + 3
+            k = chr((k - ord('A')) % (ord('Z') - ord('A') + 1) + ord('A'))
+            res += k
+        elif ord('a') <= ord(c) <= ord('z'):
+            k = ord(c) + 3
+            k = chr((k - ord('a')) % (ord('z') - ord('a') + 1) + ord('a'))
+            res += k
+        else:
+            res += c
+    return res
+
